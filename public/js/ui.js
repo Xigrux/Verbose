a30323dd4249de4d5bad6934c8f58fcd3f3281b5
Xigrux <xing.jingyuan@outlook.com>
2019-03-17 03:10:54 -0400

    stamp mechanic working

--- public/js/ui.js
+++ public/js/ui.js
@@ -13,6 +13,7 @@ interact(".draggable").draggable({
   // keep the element within the area of it's parent
   modifiers: [
     interact.modifiers.restrict({
+      restriction: "parent",
       endOnly: true,
       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
     })
@@ -25,6 +26,71 @@ interact(".draggable").draggable({
   onmove: dragMoveListener,
   // call this function on every dragend event
   onend: dragEndListener
+});
+
+/* The dragging code for '.draggable' from the demo above
+ * applies to this demo as well so it doesn't have to be repeated. */
+
+// enable draggables to be dropped into this
+interact(".dropzone").dropzone({
+  // only accept elements matching this CSS selector
+  accept: ".draggable",
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.05,
+
+  // listen for drop related events:
+
+  ondropactivate: function(event) {
+    // add active dropzone feedback
+    event.target.classList.add("drop-active");
+  },
+  ondragenter: function(event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add("drop-target");
+    draggableElement.classList.add("can-drop");
+  },
+  ondragleave: function(event) {
+    // remove the drop feedback style
+    event.target.classList.remove("drop-target");
+    event.relatedTarget.classList.remove("can-drop");
+  },
+  ondrop: function(event) {
+    var real = event.relatedTarget.classList.contains("real");
+    if (real) {
+      event.target.classList.add("stamped-real");
+    } else {
+      event.target.classList.add("stamped-fake");
+    }
+
+    var files = document.querySelectorAll(".files");
+    // files.classList.add("poop");
+    console.log(files);
+    for (var i = 2; i >= 0; i--) {
+      files[i].classList.add("retract");
+    }
+  },
+  ondropdeactivate: function(event) {
+    // remove active dropzone feedback
+    event.target.classList.remove("drop-active");
+    event.target.classList.remove("drop-target");
+  }
+});
+
+interact(".drag-drop").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrict({
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    })
+  ],
+  autoScroll: true,
+  // dragMoveListener from the dragging demo above
+  onmove: dragMoveListener
 });
 
 function dragStartListener(event) {
